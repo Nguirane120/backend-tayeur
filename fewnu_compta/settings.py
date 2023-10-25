8093662bde448beb2d318f34938e44dac03126c3
“Nguirane120” <nguiranedev120@gmail.com>
2023-10-25 15:25:10 +0000

    init db

--- fewnu_compta/settings.py
+++ fewnu_compta/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import dj_database_url
+
 # from firebase_admin import credentials
 import os
 
@@ -17,6 +18,7 @@ from dotenv import load_dotenv
 import django
 import os
 import json
+
 # import firebase_admin
 # from firebase_admin import credentials
 # Load environment variables from .env file
@@ -30,12 +32,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yjbae$vgtfdjk3y1)er)m4n(*9)4f528owz8*vm6p*du3qj(+p'
+SECRET_KEY = "yjbae$vgtfdjk3y1)er)m4n(*9)4f528owz8*vm6p*du3qj(+p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # cred = credentials.Certificate(os.path.join(BASE_DIR, "firebase/credentials.json"))
 # firebase_admin.initialize_app(cred)
@@ -43,56 +45,50 @@ ALLOWED_HOSTS = ['*']
 # django.setup()
 
 
-
-
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'django_rest_passwordreset',
-    'drf_yasg',
-    'drf_yasg2',
-    'api_fewnu_compta.apps.ApiFewnuComptaConfig',
-    'whitenoise.runserver_nostatic',
+    "jazzmin",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "django_rest_passwordreset",
+    "drf_yasg",
+    "drf_yasg2",
+    "api_fewnu_compta.apps.ApiFewnuComptaConfig",
+    "whitenoise.runserver_nostatic",
     "corsheaders",
     "backoffice",
-    'django_extensions',
+    "django_extensions",
     "gestion_horaire",
 ]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
 
 
 GRAPH_MODELS = {
-  'all_applications': True,
-  'group_models': True,
+    "all_applications": True,
+    "group_models": True,
 }
 
 GRAPH_MODELS = {
-  'app_labels': ["api_fewnu_compta.apps.ApiFewnuComptaConfig"],
+    "app_labels": ["api_fewnu_compta.apps.ApiFewnuComptaConfig"],
 }
-
-
-
-
 
 
 # cred = credentials.Certificate(os.path.join(BASE_DIR, "firebase/credentials.json"))
@@ -107,26 +103,25 @@ GRAPH_MODELS = {
 #     "http://127.0.0.1:8000",
 # ]
 
-ROOT_URLCONF = 'fewnu_compta.urls'
+ROOT_URLCONF = "fewnu_compta.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'fewnu_compta.wsgi.application'
-
+WSGI_APPLICATION = "fewnu_compta.wsgi.application"
 
 
 # Database
@@ -134,20 +129,26 @@ WSGI_APPLICATION = 'fewnu_compta.wsgi.application'
 # for herokuapp
 # DATABASES = { 'default': dj_database_url.config() }
 
-# for local 
+# for local
+# DATABASES = {
+#   'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ['DB_NAME'],
+#         'USER': os.environ['DB_USER'],
+#         'PASSWORD': os.environ['DB_PASSWORD'],
+#         'HOST': os.environ['DB_HOST'],
+#         'PORT': os.environ['DB_PORT'],
+#     }
+
+# }
 DATABASES = {
-  'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
-    }
-    
+    "default": dj_database_url.config(
+        default="postgres://tayeur_gestion_user:3QwZNBSGDlotcSFzfGoan2JLtsH8I7Hg@dpg-cksituprfc9c738f3bq0-a/tayeur_gestion",
+    )
 }
 
-# for docker 
+
+# for docker
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -158,7 +159,7 @@ DATABASES = {
 #         'PORT': 5432,
 #     }
 # }
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # SESSION_COOKIE_HTTPONLY = True
 
 # Password validation
@@ -166,30 +167,30 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -197,14 +198,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_URL = 'static/'
-AUTH_USER_MODEL = 'api_fewnu_compta.User'
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') #le chemin du serveur pour stocker les fichiers sur l’ordinateur. 
-MEDIA_URL = '/media/'# comment l’URL de référence permettant au navigateur d’accéder aux fichiers via Http.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "static/"
+AUTH_USER_MODEL = "api_fewnu_compta.User"
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, "media"
+)  # le chemin du serveur pour stocker les fichiers sur l’ordinateur.
+MEDIA_URL = "/media/"  # comment l’URL de référence permettant au navigateur d’accéder aux fichiers via Http.
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-# for cors 
+# for cors
 CSRF_HEADER_NAME = "HTTP_X_CSRFTOKEN"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
@@ -228,7 +231,7 @@ CORS_ALLOW_HEADERS = [
     "x-csrf-token",
     "csrftoken",
     "x-requested-with",
-    "http_x_csrftoken"
+    "http_x_csrftoken",
 ]
 
 # CSRF_COOKIE_SECURE = False
@@ -258,7 +261,7 @@ CORS_ALLOW_CREDENTIALS = True
 MIDDLEWARE_CLASSES = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "corsheaders.middleware.CorsPostCsrfMiddleware"
+    "corsheaders.middleware.CorsPostCsrfMiddleware",
 ]
 # SESSION_COOKIE_DOMAIN = "*.herokuapp.com"
 # CSRF_COOKIE_NAME = "csrftoken"
@@ -273,11 +276,11 @@ CSRF_COOKIE_NAME = "XSRF-TOKEN"
 # }
 
 
-# SMTP Configuration 
+# SMTP Configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'passpartoutsn@gmail.com'
-EMAIL_HOST_PASSWORD ='beoxaowspyrzbmgm'
+EMAIL_HOST_USER = "passpartoutsn@gmail.com"
+EMAIL_HOST_PASSWORD = "beoxaowspyrzbmgm"
