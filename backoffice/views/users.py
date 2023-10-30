741695411339026fa5890a643c54e0c4420ba0a8
“Nguirane120” <nguiranedev120@gmail.com>
2023-10-30 14:34:12 +0000

    changes in dashboard

--- backoffice/views/users.py
+++ backoffice/views/users.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from api_fewnu_compta.models import User
+from api_fewnu_compta.models import User,Customer,Commande
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -23,7 +23,23 @@ def usersList(request):
 @user_passes_test(is_admin)
 def userDetail(request, id):
     user = User.objects.get(id=id)
-    context = {"user":user}
+    num_clients = Customer.objects.filter(createdBy=user).count()
+    num_commandes = Commande.objects.filter(createdBy=user).count()
+    clients = Customer.objects.filter(createdBy=user)
+    commandes = Commande.objects.filter(createdBy=user)
+    clients_dates_ajout = [client.created_at for client in clients]
+    commandes_dates_ajout = [commande.date_commande for commande in commandes]
+
+    num_clients = len(clients)
+    num_commandes = len(commandes)
+    clients_dates_str = ", ".join([str(date) for date in clients_dates_ajout])
+    commandes_dates_str = ", ".join([str(date) for date in commandes_dates_ajout])
+    clients_dates_list = clients_dates_ajout
+    commandes_dates_list = commandes_dates_ajout
+
+
+    context = {"user":user, "num_clients":num_clients, "num_commandes":num_commandes, "clients_dates_ajout":clients_dates_list, "commandes_dates_ajout":commandes_dates_list}
+
     return render(request, "backoffice/users/userDetail.html",  context)
 
 
